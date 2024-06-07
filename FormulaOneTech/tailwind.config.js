6ad312ebb84bfe764a890ac48b5ca5faba23897c
Wai Ho Vong <waihovong@gmail.com>
2024-06-07 14:02:35 +0930

    Added drivers standing card and included styling to card layout. collapsable

--- FormulaOneTech/tailwind.config.js
+++ FormulaOneTech/tailwind.config.js
@@ -1,10 +1,13 @@
 module.exports = {
     content: ["./**/.{razor,html,cshtml}",
         "C:/Users/61434/source/repos/FormulaOneTech/FormulaOneTech/Components/**/*.razor",
-        "C:\Users\61434\source\repos\FormulaOneTech\FormulaOneTech\Components\Pages\LandingPage.razor"
     ],
     theme: {
-    extend: {},
-  },
-  plugins: [],
+        extend: {
+            fontFamily: {
+                'sans': ['Poppins', 'sans-serif'],
+            },
+        },
+    },
+    plugins: [],
 }
